e8bee03bf1f880d49c14230cfe6a63be6475b0ff
SaurabhOllato <saurabh.ollato@gmail.com>
2025-06-12 14:07:55 +0530

    no changes

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -385,6 +385,7 @@ export default function Collection() {
           <div className="flex justify-center items-center py-12">
             <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-black"></div>
           </div>
+        
         )}
 
         {/* Category Filters */}
